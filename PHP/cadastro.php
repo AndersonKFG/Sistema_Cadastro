2e2da23f1c89698292ccb99f04d256f606c9b440
Anderson Kauan <anderson.kfgoveia@gmail.com>
2023-03-06 19:52:20 -0300

    Versão 2 - Erro em verificação de cadastro. (cadastro.php)

--- PHP/cadastro.php
+++ PHP/cadastro.php
@@ -2,10 +2,21 @@
 
     include __DIR__.'\conector.php';
 
-    $nome = $_GET['nome'];  
-    $senha = $_GET['senha'];
-    $email = $_GET['email'];
-    $data_nascimento = $_GET['data_nascimento'];
+    $email = "thekingof@gmail.com";
 
-    mysqli_query($conexao, "INSERT INTO cadastro(nome,senha,email,data_nascimento)
-    VALUES ('$nome','$senha','$email','$data_nascimento')");
+    // $nome = $_GET['nome'];  
+    // $senha = $_GET['senha'];
+    // $email = $_GET['email'];
+    // $data_nascimento = $_GET['data_nascimento'];
+
+    $emails = mysqli_query($conexao, "SELECT email FROM cadastro WHERE email = $email ");
+
+    if ($emails->num_rows == 1) {
+        echo "Email já existente.";
+    }
+    elseif ($emails->num_rows == 0){
+        echo "Email cadastrado com sucesso.";
+    }
+
+    // mysqli_query($conexao, "INSERT INTO cadastro(nome,senha,email,data_nascimento)
+    // VALUES ('$nome','$senha','$email','$data_nascimento')");
--- PHP/conector.php
+++ PHP/conector.php
@@ -1,18 +1,18 @@
 <?php
 
-    /*
+    
     $dbhost = "localhost";
     $dbUsername = "root";
     $dbPassword = "";
     $dbName = "apashe";
     $port = "3306";
-    */
     
-    $dbhost = "127.0.0.1";
-    $dbUsername = "Aluno";
-    $dbPassword = "aluno";
-    $dbName = "apashe";
-    $port = "3306";
+    
+    // $dbhost = "127.0.0.1";
+    // $dbUsername = "Aluno";
+    // $dbPassword = "aluno";
+    // $dbName = "apashe";
+    // $port = "3306";
     
     
     $conexao = mysqli_connect($dbhost, $dbUsername, $dbPassword, $dbName, $port);
